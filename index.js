00a4d6be097a9ce6d05f3ed2de1600327365001b
Janne Saarinen <janne.saarinen@mail.com>
2020-01-21 19:56:46 +0200

    commit

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  const persons = persons.filter(person => person.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   res.status(204).end()
 })
