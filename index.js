411e37cb6fdb0bfe94c824254df838e9da31f460
Janne Saarinen <janne.saarinen@mail.com>
2020-01-24 01:26:56 +0200

    uibuild

--- index.js
+++ index.js
@@ -19,11 +19,6 @@ if ( process.argv.length<3 ) {
   process.exit(1)
 }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
-
 const url = `mongodb+srv://Punttikarhu:${password}@cluster0-diflf.mongodb.net/test?retryWrites=true&mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
