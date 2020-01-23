e2607b590ec902007fd52dd8359ef9812bf14862
Janne Saarinen <janne.saarinen@mail.com>
2020-01-24 00:40:42 +0200

    uibuild

--- index.js
+++ index.js
@@ -12,6 +12,52 @@ let persons = [
   }
 ]
 
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+
+const url = `mongodb+srv://Punttikarhu:${password}@cluster0-diflf.mongodb.net/test?retryWrites=true&mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (!name) {
+  Person.find({}).then(result => {
+    console.log("Phonebook:")
+    result.forEach(person => {
+      console.log(person)
+    })
+  })
+    .then(response => mongoose.connection.close())
+    
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+    date: new Date()
+  })  
+
+  person.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
 
 app.use(express.static('build'))
 app.use(cors())
@@ -56,12 +102,14 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-app.get('/', (req, res) => {
-  res.send('<h1>hello world!</h1>')
-})
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(result => {
+    console.log("Phonebook:")
+    result.forEach(person => {
+      console.log(person)
+    })
+  })
+    .then(response => mongoose.connection.close())
 })
 
 app.get('/api/persons/:id', (req, res) => {
