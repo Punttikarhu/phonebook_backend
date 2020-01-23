0702af3fe81c940cb4675a1165a52999f6602237
Janne Saarinen <janne.saarinen@mail.com>
2020-01-24 01:04:13 +0200

    uibuild

--- index.js
+++ index.js
@@ -11,31 +11,6 @@ let persons = [
     "id": 1
   }
 ]
-// Mongoose
-const mongoose = require('mongoose')
-
-if ( process.argv.length<3 ) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
-
-const url = `mongodb+srv://Punttikarhu:${password}@cluster0-diflf.mongodb.net/test?retryWrites=true&mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-  date: Date,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
 
 
 app.use(express.static('build'))
@@ -82,10 +57,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    response.json(persons)  
-  })
-    .then(response => mongoose.connection.close())
+  response.json(persons)  
 })
 
 app.get('/api/persons/:id', (req, res) => {
