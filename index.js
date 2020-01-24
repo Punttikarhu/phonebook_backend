0fdb656ba900182a208eb5353805c96891ba734e
Janne Saarinen <janne.saarinen@mail.com>
2020-01-24 21:32:16 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
