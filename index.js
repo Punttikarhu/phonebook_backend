9573577911236241ddf165d84e0568e8eb09a221
Janne Saarinen <janne.saarinen@mail.com>
2020-01-24 23:14:08 +0200

    uibuild

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-
 console.log('connecting to', url)
-console.log(typeof url)
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(String(url), { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     console.log('connected to MongoDB')
   })
