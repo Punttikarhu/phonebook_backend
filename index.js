9f674c55fb31cb70d07857e3ca1d2da11e785a08
Janne Saarinen <janne.saarinen@mail.com>
2020-01-25 21:24:20 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config()
 console.log(process.env.MONGODB_URI)
 const express = require('express')
 const bodyParser = require('body-parser')
